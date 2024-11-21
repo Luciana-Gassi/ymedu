13fd01ee6f47b9fbb5b7ced1cbd33b1a52dac2ec
Luciana-Gassi <luciana.gassi@gmail.com>
2024-11-21 17:01:29 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				controllerfour: ["ControllerFour", "sans-serif"],
+				orbitron: ["Orbitron", "sans-serif"],
+				proximanova: ["Proxima Nova", "sans-serif"],
+			},
+			colors: {
+				"ymf-primary": "#1B365D",
+				"ymf-secondary": "#228B22",
+				"ymf-primary-light": "#2C5494",
+				"ymf-secondary-light": "#2EA62E",
+				"ymf-gray": "#4a4a4a",
+				"ymf-light": "#f8f9fa",
+			},
+		},
+	},
+	plugins: [],
+};
